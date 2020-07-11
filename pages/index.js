699de34799f141beb895adda695496516e1df55c
FreddyPoly <frederic@wecraftapps.com>
2020-07-11 19:12:45 +0200

    Nouveau layout

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="container">
       <section className="hero">
-        <img src="https://images.unsplash.com/photo-1526816229784-65d5d54ac8bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
+        <img src="https://www.tourismebretagne.com/app/uploads/crt-bretagne/2018/11/suscinio-et-la-presqu-ile-de-rhuys-5-1920x960-crop-1542114215.jpg" />
 
         <div>
           <p>Domaine de Suscinio</p>
@@ -10,15 +10,25 @@ const Home = () => {
       </section>
 
       <section className="introduction">
-        <h2>Un château, des animations et une nature préservée</h2>
+        <h2>Suscinio: le domaine des ducs de Bretagne</h2>
 
         <div className="separator"></div>
 
         <p>Patrimoine de Bretagne, le château de Suscinio est un incontournable du Golfe du Morbihan. Situé au milieu d'un parc naturel d'une richesse et d'une diversité d'activités rares, il propose une palette d'animations historiques unique... A quelques minutes à pieds de la plus belle plage.</p>
       </section>
 
+      <section className="presentation">
+        <img src="https://www.suscinio.fr/wp-content/uploads/sites/37/2020/03/rabouan_XT22102.jpg" />
+
+        <p>C’est dans un environnement magnifique, sur la presqu’île de Rhuys, au bord de la mer, au milieu d’Espaces Naturels Sensibles que s’élance l’énigmatique château de Suscinio.  Prieuré au XIIIe siècle, autour duquel s’est développé un domaine agricole, il s’agrandit sous l’impulsion des Ducs de Bretagne qui y implantent un premier manoir.
+        Remanié et agrandi, il deviendra le relais de chasse favori des têtes couronnées. Classé Monument Historique par Prosper Mérimée en 1840, il est aujourd’hui la propriété du Département du Morbihan qui assure sa restauration depuis plus de 50 ans.</p>
+      </section>
+
+      <p className="introduction-next">Le château de Suscinio, résidence des ducs de Bretagne, se dresse au bord de la mer dans la commune de Sarzeau (Morbihan), au cœur de ce qui était autrefois un vaste espace boisé. Construit à la fin du Moyen Âge, il est situé dans la presqu'île de Rhuys, aujourd'hui formé de marais salants et de prairies. Le château s’inscrit dans la lignée des grandes résidences ducales émergeant alors en Bretagne sous l’impulsion de ducs puissants.
+      Longtemps laissé à l’état de ruine, le château est finalement classé en 1840 après la visite de Prosper Mérimée, cinq ans plus tôt, qui le met sur la première liste des Monuments historiques. À l’époque, le château est visité pour le caractère romantique de ses ruines.</p>
+
       <section className="reservation">
-        <button>Réserver un créneau</button>
+        <h3>Découvrez nos escapes game !</h3>
       </section>
 
       <section className="events">
@@ -34,6 +44,8 @@ const Home = () => {
 
             <p>Enfermé dans une pièce secrète du château de Suscinio, partez à la chasse aux indices et résolvez les différentes énigmes pour vous échapper à temps de la chambre des mystères. Bon en arrière au XVIIIe siècle, à l’époque de la célèbre affaire du collier de la reine.
             Tout le royaume de France cherche les coupables. Le principal suspect est en prison, sur le point d’être jugé, lorsque vous recevez une lettre mystérieuse du prince de Condé, qui soupçonne la comtesse de la Motte d’avoir commandité l’escroquerie. Fouillez la chambre des mystères pour trouver des preuves qui innocenteront le suspect. Une véritable course contre le temps s’engage, à vous de jouer !</p>
+
+            <button>En savoir plus</button>
           </div>
         </div>
 
@@ -45,12 +57,18 @@ const Home = () => {
 
             <p>Au château de Suscinio, faites un voyage au XVIe siècle pour vivre un évènement historique ! Vous devrez infiltrer les partisans du roi Henri III. Logique, esprit d’équipe et observation seront vos principales armes pour déjouer un guet-apens royal orchestré par le roi lui-même pour assassiner son principal rival, le duc de Guise.
             Parviendrez-vous à changer l’Histoire et à contrecarrer les plans du roi ? Vous aurez 60 minutes pour vivre un des évènements les plus marquants du château de Suscinio…</p>
+
+            <button>En savoir plus</button>
           </div>
         </div>
       </section>
 
-      <section className="reservation">
-        <button>Réserver un créneau</button>
+      <section className="contact">
+        <h2>Nous contacter</h2>
+
+        <div className="separator"></div>
+
+        <p>Patrimoine de Bretagne, le château de Suscinio est un incontournable du Golfe du Morbihan. Situé au milieu d'un parc naturel d'une richesse et d'une diversité d'activités rares, il propose une palette d'animations historiques unique... A quelques minutes à pieds de la plus belle plage.</p>
       </section>
 
       <section className="footer">
