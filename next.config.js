b78675d054820a6d4026942e197bf898332bb3a6
FreddyPoly <frederic@wecraftapps.com>
2020-07-12 18:45:08 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  // Target must be serverless
-  target: 'serverless'
-};
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/enigme/chambre-des-mysteres": {
+        page: "/enigme/[slug]",
+        query: { slug: "chambre-des-mysteres" }
+      },
+      "/enigme/guet-apens-royal": {
+        page: "/enigme/[slug]",
+        query: { slug: "guet-apens-royal" }
+      }
+    }
+  }
+}
