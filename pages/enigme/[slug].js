c062cb72494f9fef016bd7ac408115d5df55b01a
FreddyPoly <frederic@wecraftapps.com>
2020-07-12 17:58:03 +0200

    Link au lieu de button

--- pages/enigme/[slug].js
+++ pages/enigme/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const Enigma = () => {
   const router = useRouter();
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const Home = () => {
   const router = useRouter();
@@ -52,6 +53,10 @@ const Home = () => {
 
             <p>Enfermé dans une pièce secrète du château de Suscinio, partez à la chasse aux indices et résolvez les différentes énigmes pour vous échapper à temps de la chambre des mystères. Bon en arrière au XVIIIe siècle, à l’époque de la célèbre affaire du collier de la reine.
             Tout le royaume de France cherche les coupables. Le principal suspect est en prison, sur le point d’être jugé, lorsque vous recevez une lettre mystérieuse du prince de Condé, qui soupçonne la comtesse de la Motte d’avoir commandité l’escroquerie. Fouillez la chambre des mystères pour trouver des preuves qui innocenteront le suspect. Une véritable course contre le temps s’engage, à vous de jouer !</p>
+            
+            <Link href="/enigme/chambre-des-mysteres">
+              <a>Home</a>
+            </Link>
 
             <button type="button" onClick={() => navigateToEscapeGame('chambre-des-mysteres')}>En savoir plus</button>
           </div>
